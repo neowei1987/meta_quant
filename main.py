afde55c30b83ae2934069689b0669ab47a6e4377
neowei1987 <linfeng303@126.com>
2023-05-08 23:55:07 +0800

    add

---
+++ main.py
@@ -0,0 +1,40 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+# 技术面
+# 趋势方向如何
+# 涨跌空间大小
+# 有无买/卖信号
+
+
+# 资金面
+# 大盘/板块资金流向
+# 个股资金流向
+# 主力大单的表现 【影响很大】
+
+# 基本面，选股有用
+# 业绩表现
+# 潜力大小
+# 是否抗跌
+
+# 消息面
+# 市场情绪
+# 利多/利空的消息/新闻
+# 公告事件
+
+# 数据
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
